f984909c46a0f8614193107c50a51442c414aefc
Your Name <you@example.com>
2022-04-06 22:45:19 +0700

    init commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Time Tracker",
+      theme: ThemeData(
+        primarySwatch: Colors.indigo
+      ),
+      home: Container(
+        color: Colors.white,
+      ),
+    );
+  }
+}
